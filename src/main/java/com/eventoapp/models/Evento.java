939e3af64b12f9318f7c043bb6996480c3b9c4cd
crispimluiz <crispimluiz@live.com>
2019-05-30 09:59:40 -0300

    Atualização

---
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -0,0 +1,61 @@
+package com.eventoapp.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Codigo;
+	private String nome;
+	private String local;
+	private String data;
+	private String horario;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+	public Long getCodigo() {
+		return Codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		Codigo = codigo;
+	}
+
+}
